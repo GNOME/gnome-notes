45e77b6fbdd46873ef9de7e1b8caf41a22a2c6d0
Pierre-Yves Luyten <py@luyten.fr>
2013-03-13 23:59:12 +0100

    License: use consistently GPLv3+
    Some files had GPLv2+.
    Also add the license to files without. This does not impact bjb-editor-*
    Obviously this does not impact neither libgd

--- src/bijiben-shell-search-provider.c
+++ src/bijiben-shell-search-provider.c
@@ -2,23 +2,22 @@
  * bijiben-shell-search-provider.c - Implementation of a GNOME Shell
  *   search provider
  *
- * Copyright (C) 2012 Red Hat, Inc.
+ * Copyright © 2012, 2013 Red Hat, Inc.
+ * Copyright © 2013 Pierre-Yves LUYTEN <py@luyten.fr>
  *
- * Bijiben is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
+ * bijiben is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Bijiben is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
+ * bijiben is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
-*/
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /*
  * Adapted from Nautilus shell-search-provider

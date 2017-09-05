3940b99f479432c8e53dd5e3d2353621834aa042
Iñigo Martínez <inigomartinez@gmail.com>
2017-09-05 17:31:03 +0200

    build: Rename the names of the icons' files
    The names of icons' files, besides containing the final names, they
    also contained the path in which they were going to be installed.
    This implies that they have to be renamed on the installation
    process.
    
    This patch creates the proper directory tree needed on the
    installation process and renames all files' names to their final
    names.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=787317

--- meson_post_install.py
+++ meson_post_install.py
@@ -1,25 +1,13 @@
 #!/usr/bin/env python3
 
-import glob
 import os
-import re
 import subprocess
 import sys
 
 if not os.environ.get('DESTDIR'):
   datadir = sys.argv[1]
 
-  name_pattern = re.compile('hicolor_(?:apps|actions)_(?:\d+x\d+|scalable)_(.*)')
-  search_pattern = '/**/hicolor_*'
-
-  actions_icondir = os.path.join(datadir, 'bijiben', 'icons', 'hicolor')
-  [os.rename(file, os.path.join(os.path.dirname(file), name_pattern.search(file).group(1)))
-   for file in glob.glob(actions_icondir + search_pattern, recursive=True)]
-
   icondir = os.path.join(datadir, 'icons', 'hicolor')
-  [os.rename(file, os.path.join(os.path.dirname(file), name_pattern.search(file).group(1)))
-   for file in glob.glob(icondir + search_pattern, recursive=True)]
-
   print('Update icon cache...')
   subprocess.call(['gtk-update-icon-cache', '-f', '-t', icondir])
 

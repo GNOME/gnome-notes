256f71ac4c639732000698d6991032d46ac53b44
Isaque Galdino <igaldino@gmail.com>
2019-09-03 17:33:23 -0300

    date-time: GTimeVal Deprecation

--- src/libbiji/biji-date-time.h
+++ src/libbiji/biji-date-time.h
@@ -25,4 +25,4 @@ const gchar      *biji_get_time_diff_with_time            (glong sec_since_epoch
 
 
 
-gint64            iso8601_to_gint64                       (gchar *iso8601);
+gint64            iso8601_to_gint64                       (const char *iso8601);

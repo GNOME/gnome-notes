3bdc89e309f5b44b82d4fc974fd2e8f2d555f8d9
Jonathan Kang <jonathankang@gnome.org>
2020-02-21 15:25:10 +0800

    window-base: Add "<Primary>w" shortcut to close window.
    Add this convenient accelerator to close windows.
    
    https://gitlab.gnome.org/GNOME/gnome-notes/issues/145

--- src/bjb-application.c
+++ src/bjb-application.c
@@ -390,6 +390,7 @@ bijiben_startup (GApplication *application)
   g_autoptr(GError) error = NULL;
   GdkRGBA         color = {0,0,0,0};
 
+  const gchar *vaccels_close[] = {"<Primary>w", NULL};
   const gchar *vaccels_detach[] = {"<Primary>d", NULL};
   const gchar *vaccels_redo[] = {"<Primary><Shift>z", NULL};
   const gchar *vaccels_undo[] = {"<Primary>z", NULL};
@@ -400,7 +401,8 @@ bijiben_startup (GApplication *application)
 
   bjb_apply_style ();
 
-  gtk_application_set_accels_for_action (GTK_APPLICATION (application), "win.detach-window", vaccels_detach);	
+  gtk_application_set_accels_for_action (GTK_APPLICATION (self), "win.close", vaccels_close);
+  gtk_application_set_accels_for_action (GTK_APPLICATION (application), "win.detach-window", vaccels_detach);
   gtk_application_set_accels_for_action (GTK_APPLICATION (application), "win.redo", vaccels_redo);
   gtk_application_set_accels_for_action (GTK_APPLICATION (application), "win.undo", vaccels_undo);
   gtk_application_set_accels_for_action (GTK_APPLICATION (application), "win.trash", vaccels_trash);

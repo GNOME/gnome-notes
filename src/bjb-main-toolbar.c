118302839860e2c3899754456d1f11e1858b8665
Jonathan Kang <jonathankang@gnome.org>
2020-02-26 10:54:35 +0800

    main-toolbar: Fix back button not showing in archive view
    If you go to trash from main view and open a note, then click the back
    button to go back to trash, you won’t be able to navigate to main view
    any more. The back button in the toolbar is missing.
    
    Fix that here.

--- src/bjb-main-toolbar.c
+++ src/bjb-main-toolbar.c
@@ -314,9 +314,17 @@ on_back_button_clicked (BjbMainToolbar *self)
 
   if (self->note)
     {
+      if (biji_note_obj_is_trashed (self->note))
+      {
+        bjb_window_base_switch_to (BJB_WINDOW_BASE (self->window),
+                                   BJB_WINDOW_BASE_ARCHIVE_VIEW);
+      }
+      else
+      {
+        bjb_window_base_switch_to (BJB_WINDOW_BASE (self->window),
+                                   BJB_WINDOW_BASE_MAIN_VIEW);
+      }
       disconnect_note_handlers (self);
-      bjb_window_base_switch_to (BJB_WINDOW_BASE (self->window),
-                                 BJB_WINDOW_BASE_MAIN_VIEW);
       bjb_main_view_update_model (self->parent);
       return;
     }
